dc1a6109afd8fd295b7d7c15778763e7ba8f5a36
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-05-01 21:49:23 +0700

    feat: Add view-all-categories page

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,7 +1,32 @@
-/*
-    GET /
-    homepage
-*/
-exports.homepage = (req, res) => {
-    res.render("index", { title: "Cooking Blog - Home" });
+require("../models/database");
+const categorySchema = require("../models/Category");
+
+exports.homepage = async (req, res) => {
+
+    const limitNumber = 5;
+    const categories = await categorySchema.find().limit(limitNumber);
+
+    try {
+        res.render("index", {
+            title: "Cooking Blog - Kiero",
+            categories
+        })
+    } catch (err) {
+        res.status(500).send({ error: err })
+    }
 }
+
+exports.exploreCategories = async (req, res) => {
+
+    const categories = await categorySchema.find();
+
+    try {
+        res.render("categories", {
+            title: "Cooking Blog - Categories",
+            categories
+        })
+    } catch (err) {
+        res.status(500).send({ error: err })
+    }
+}
+
