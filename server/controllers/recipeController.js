8c05d4199b3298ba1d6a9dd865fb9f2f277fa22f
Kiero1211 <96629974+Kiero1211@users.noreply.github.com>
2024-05-02 18:06:04 +0700

    feat: Add recipes into landing page anh individual recipe page

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,16 +1,33 @@
 require("../models/database");
-const categorySchema = require("../models/Category");
-const recipeSchema = require("../models/Recipe");
+const Category = require("../models/Category");
+const Recipe = require("../models/Recipe");
 
 exports.homepage = async (req, res) => {
 
     const limitNumber = 5;
-    const categories = await categorySchema.find().limit(limitNumber);
+    const categories = await Category.find().limit(limitNumber);
+    const Latest = await Recipe.find().sort({ "-id": -1 }).limit(limitNumber);
+    const Thai = await Recipe.find({ category: "Thai" }).limit(limitNumber);
+    const American = await Recipe.find({ category: "American" }).limit(limitNumber);
+    const Chinese = await Recipe.find({ category: "Chinese" }).limit(limitNumber);
+    const Italian = await Recipe.find({ category: "Italian" }).limit(limitNumber);
+    const Mexican = await Recipe.find({ category: "Mexican" }).limit(limitNumber);
+    const Indian = await Recipe.find({ category: "Indian" }).limit(limitNumber);
 
+    const recipes = {
+        Latest,
+        Thai,
+        American,
+        Chinese,
+        Italian,
+        Mexican,
+        Indian
+    };
     try {
         res.render("index", {
             title: "Cooking Blog - Kiero",
-            categories
+            categories,
+            recipes
         })
     } catch (err) {
         res.status(500).send({ error: err })
@@ -18,8 +35,7 @@ exports.homepage = async (req, res) => {
 }
 
 exports.exploreCategories = async (req, res) => {
-
-    const categories = await categorySchema.find();
+    const categories = await Category.find();
 
     try {
         res.render("categories", {
@@ -27,7 +43,21 @@ exports.exploreCategories = async (req, res) => {
             categories
         })
     } catch (err) {
-        res.status(500).send({ error: err })
+        res.status(500).send({ error: err.message });
+    }
+}
+
+exports.exploreRecipe = async (req, res) => {
+    const requestedId = req.params.id;
+
+    const recipe = await Recipe.findById(requestedId);
+    try {
+        res.render("recipe", {
+            title: "Cooking Blog - Recipe",
+            recipe
+        })
+    } catch (err) {
+        res.status(500).send({ error: err.message });
     }
 }
 
--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -6,7 +6,18 @@ const recipeController = require("../controllers/recipeController");
     App Routes
 */
 router.get("/", recipeController.homepage);
+
+/*
+    GET /categories
+    Categories
+*/
 router.get("/categories", recipeController.exploreCategories);
+
+/*
+    GET /recipes
+    Recipes
+*/
+router.get("/recipe/:id", recipeController.exploreRecipe);
 
 
 module.exports = router;
